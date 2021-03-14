0e6306e89c85c9bde98e4da588f812bad2214f74
TUHINPRO <71925866+TUHINPRO@users.noreply.github.com>
2021-03-14 12:30:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+Webcam.set({
+    width:300,
+    height:300,
+    image_format:'jpg',
+    jpg_quality:90,
+     
+    constraints:{
+        facingMode:"environment"
+    }
+});
+
+
+
+Webcam.attach('#cameraUwU');
+
+
